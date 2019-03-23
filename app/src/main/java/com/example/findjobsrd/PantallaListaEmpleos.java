3550894ae61af7535872aa683baae0177e03e037
Lifer Mayi <lifermayi23@gmail.com>
2019-03-23 14:08:25 -0400

    Inicio del diseño de la pantalla, resultado despues de la busqueda, lista de empleos

---
+++ app/src/main/java/com/example/findjobsrd/PantallaListaEmpleos.java
@@ -0,0 +1,13 @@
+package com.example.findjobsrd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantallaListaEmpleos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_lista_empleos);
+    }
+}
